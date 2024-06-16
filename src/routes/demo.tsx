8bbf9a77c3520c60cfb7dbc198e15665ca8d8869
Mickael Bobovitch <m.bobovitch@gmail.com>
2024-06-16 09:58:25 +0200

    fix

--- src/routes/demo.tsx
+++ src/routes/demo.tsx
@@ -8,39 +8,15 @@ export const Route = createFileRoute("/demo")({
 function Demo() {
     const [image, setImage] = useState(null);
 
+    // fetch server every 1s
     useEffect(() => {
-        const socket = new WebSocket("ws://10.68.249.219:65432");
+        const interval = setInterval(() => {
+            fetch("http://0.0.0.0:8000/").then((res) => {
+                console.log("coucou");
+            });
+        }, 1000);
 
-        socket.onopen = () => {
-            console.log("WebSocket connection established");
-        };
-
-        socket.onmessage = (event) => {
-            // const data = event.data;
-            // if (data instanceof Blob) {
-            //     const url = URL.createObjectURL(data);
-            //     setImage(url);
-            // }
-        };
-
-        socket.onclose = () => {
-            console.log("WebSocket connection closed");
-        };
-
-        socket.onerror = (error) => {
-            console.error("WebSocket error:", error);
-        };
-
-        const handleKeyDown = (event: any) => {
-            // socket.send(JSON.stringify({ type: "keyEvent", key: event.key }));
-        };
-
-        window.addEventListener("keydown", handleKeyDown);
-
-        return () => {
-            socket.close();
-            window.removeEventListener("keydown", handleKeyDown);
-        };
+        return () => clearInterval(interval);
     }, []);
 
     return (
