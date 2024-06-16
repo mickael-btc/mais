4f8ee90e63c8a6cc48ac2ef9748bb7f83a26d490
Mickael Bobovitch <m.bobovitch@gmail.com>
2024-06-16 10:21:36 +0200

    feat: keys

--- src/routes/demo.tsx
+++ src/routes/demo.tsx
@@ -5,18 +5,63 @@ export const Route = createFileRoute("/demo")({
     component: Demo,
 });
 
+function mapKeyToDirection(key: string) {
+    switch (key) {
+        case "ArrowUp":
+            return "FRONT";
+        case "ArrowDown":
+            return "BACK";
+        case "ArrowLeft":
+            return "LEFT";
+        case "ArrowRight":
+            return "RIGHT";
+        case " ":
+            return "UP";
+        case "Shift":
+            return "DOWN";
+        default:
+            return null;
+    }
+}
+
 function Demo() {
     const [image, setImage] = useState(null);
 
-    // fetch server every 1s
     useEffect(() => {
-        const interval = setInterval(() => {
-            fetch("http://0.0.0.0:8000/").then((res) => {
-                console.log("coucou");
-            });
-        }, 1000);
+        const socket = new WebSocket("ws://yourserver.com/path");
 
-        return () => clearInterval(interval);
+        socket.onopen = () => {
+            console.log("WebSocket connection established");
+        };
+
+        socket.onmessage = (event) => {
+            const data = event.data;
+            if (data instanceof Blob) {
+                const url = URL.createObjectURL(data);
+                setImage(url);
+            }
+        };
+
+        socket.onclose = () => {
+            console.log("WebSocket connection closed");
+        };
+
+        socket.onerror = (error) => {
+            console.error("WebSocket error:", error);
+        };
+
+        const handleKeyDown = (event) => {
+            // socket.send(JSON.stringify({ type: "keyEvent", key: event.key }));
+            const direction = mapKeyToDirection(event.key);
+            console.log(direction);
+        };
+
+        window.addEventListener("keydown", handleKeyDown);
+
+        return () => {
+            socket.close();
+            window.removeEventListener("keydown", handleKeyDown);
+        };
     }, []);
 
     return (
